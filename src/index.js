7788bf614a3e36c991ab851491763a3daf41f392
kennankole <kennankole@gmail.com>
2023-06-01 17:26:20 +0300

    Modified pop up behaviour on all the table columns

--- src/index.js
+++ src/index.js
@@ -1,31 +1,83 @@
-class Modal {
-  constructor() {
-    this.tableCells = document.querySelectorAll('td');
-    this.modal = document.getElementById('modal');
-    this.addEventListeners();
-  }
+const cancelButton = document.getElementById('cancel');
+const dialog = document.getElementById('favDialog');
+// const form = document.getElementById('form-data');
 
-  addEventListeners() {
-    this.tableCells.forEach((cell) => {
-      cell.addEventListener('click', () => this.openModal(cell.className));
-    });
-    this.modal.addEventListener('click', () => this.closeModal());
+const openCheck = (dialog, className) => {
+  if (dialog.open) {
+    document.getElementById('label-text').innerHTML = `Plan your ${className} here`;
   }
+};
 
-  openModal(className) {
-    this.modal.style.display = 'flex';
-    this.modal.innerHTML = `
-      <div class="modal-content">
-        <p>Let us plan you ${className} </p>
-      </div>
-      `;
-  }
+const tableCells = document.querySelectorAll('td');
+tableCells.forEach((cells) => {
+  cells.addEventListener('click', () => {
+    dialog.showModal();
+    openCheck(dialog, cells.className);
+  });
+});
 
-  closeModal() {
-    this.modal.style.display = 'none';
-    // this.modal.innerText = 'exiting';
-  }
-}
+dialog.returnValue = 'favAnimal';
 
-// eslint-disable-next-line no-unused-vars
-const modal = new Modal('open-modal', 'close-modal', 'modal');
+cancelButton.addEventListener('click', () => {
+  dialog.close('animalNotChosen');
+  openCheck(dialog);
+});
+
+// class Modal {
+//   constructor() {
+//     this.tableCells = document.querySelectorAll('td');
+//     this.modal = document.getElementById('modal');
+//     this.addEventListeners();
+//   }
+
+//   addEventListeners() {
+//     this.tableCells.forEach((cell) => {
+//       cell.addEventListener('click', () => this.openModal(cell.className));
+//     });
+//     this.modal.addEventListener('click', () => this.closeModal());
+//   }
+
+//   openModal() {
+//     this.modal.style.display = 'flex';
+//     this.modal.innerHTML = `
+//         <h2>Fill in the details</h2>
+//         <form id="myForm">
+//           <label for="name">Name:</label>
+//           <input type="text" id="name" name="name" required>
+//           <br>
+//           <label for="email">Email:</label>
+//           <input type="emial" id="email" name="email" required>
+//           <br>
+//           <button type="submit">Submit</button>
+//         </form>
+//       `;
+//     const form = document.getElementById('myForm');
+//     form.addEventListener('submit', (e) => this.handleSubmit(e));
+
+//     const formElements = form.querySelector('input, button');
+//     formElements.forEach((element) => {
+//       element.addEventListener('click', (e) => e.stopPropagation());
+//     });
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const name = formData.get('name');
+//     const email = formData.get('email');
+
+//     console.log(`Name: ${name}, Email: ${email}`);
+//     this.closeModal();
+//   }
+
+//   closeModal() {
+//     this.modal.style.display = 'none';
+//     // this.modal.innerText = 'exiting';
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const modal = new Modal();
+// });
+// // eslint-disable-next-line no-unused-vars
+// // const modal = new Modal();
